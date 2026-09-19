29f3c6f2e33e3bc0a8082006211e7a39065aeee6
Sami Al Jaber Tasin <tasinjaber407@gmail.com>
2026-09-19 21:42:59 +0200

    feat: build form forge interface

---
+++ eslint.config.js
@@ -0,0 +1,1 @@
+import js from "@eslint/js"; import globals from "globals"; import tseslint from "typescript-eslint"; export default tseslint.config({ignores:["dist","coverage"]},js.configs.recommended,...tseslint.configs.recommended,{files:["**/*.{ts,tsx}"],languageOptions:{globals:globals.browser}});
